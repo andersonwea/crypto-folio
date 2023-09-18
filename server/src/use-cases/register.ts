c306bcbd839f01536e9ae215bda7ef6e60831462
chain <klaus.mika1052@gmail.com>
2023-09-18 12:59:14 -0300

    create regiter user use case

---
+++ server/src/use-cases/register.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExitsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  nickname: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    nickname,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const isUserAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (isUserAlreadyExists) {
+      throw new UserAlreadyExitsError()
+    }
+
+    const password_hash = await hash(password, 6)
+
+    const user = await this.usersRepository.create({
+      nickname,
+      email,
+      password_hash,
+    })
+
+    return {
+      user,
+    }
+  }
+}
