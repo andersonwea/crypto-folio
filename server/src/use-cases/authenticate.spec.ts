840b56030375a08f54593941bb23870e060bd4a4
chain <klaus.mika1052@gmail.com>
2023-09-23 14:58:56 -0300

    feat: add more tests to authenticate use case

--- server/src/use-cases/authenticate.spec.ts
+++ server/src/use-cases/authenticate.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
   let usersRepository: UsersRepository
@@ -31,5 +32,20 @@ describe('Authenticate Use Case', () => {
         nickname: 'john',
       }),
     )
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await usersRepository.create({
+      nickname: 'john',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
