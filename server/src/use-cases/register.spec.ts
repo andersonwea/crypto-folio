7aea86c62012fc2365e07e35ae9ff6495a16fecb
chain <klaus.mika1052@gmail.com>
2023-09-23 14:52:19 -0300

    feat: add more tests to register user case

--- server/src/use-cases/register.spec.ts
+++ server/src/use-cases/register.spec.ts
@@ -2,7 +2,8 @@ import { UsersRepository } from '@/repositories/users-repository'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { hash } from 'bcryptjs'
+import { compare, hash } from 'bcryptjs'
+import { UserAlreadyExitsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   let usersRepository: UsersRepository
@@ -15,16 +16,44 @@ describe('Register Use Case', () => {
 
   it('should be able to register user', async () => {
     const { user } = await sut.execute({
-      nickname: 'pipoca',
-      email: 'pipoca@example.com',
+      nickname: 'jonh',
+      email: 'johndoe@example.com',
       password: await hash('123456', 6),
     })
 
     expect(user).toEqual(
       expect.objectContaining({
-        nickname: 'pipoca',
-        email: 'pipoca@example.com',
+        nickname: 'jonh',
+        email: 'johndoe@example.com',
       }),
     )
+  })
+
+  it('should hash user password upn registration', async () => {
+    const { user } = await sut.execute({
+      nickname: 'jonh',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectHashed = await compare('123456', user.password_hash)
+
+    expect(isPasswordCorrectHashed).toBe(true)
+  })
+
+  it('should not to be able to register twice with same email', async () => {
+    await sut.execute({
+      nickname: 'jonh',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await expect(() =>
+      sut.execute({
+        nickname: 'jonh',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExitsError)
   })
 })
