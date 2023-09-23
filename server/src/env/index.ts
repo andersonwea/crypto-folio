558d762ffdb0c4225e9008dbd5ad2451ecac30d0
chain <klaus.mika1052@gmail.com>
2023-09-23 17:45:09 -0300

    chore: add api key to env

--- server/src/env/index.ts
+++ server/src/env/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  CRYPTO_RANK_API_KEY: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
