f57a38ba4fe2267e94ca0c6d537dbc063ba968a9
chain <klaus.mika1052@gmail.com>
2023-09-25 21:07:57 -0300

    feat: add method to find all transaction by currency and implements

--- server/src/repositories/in-memory/in-memory-transactions-repository.ts
+++ server/src/repositories/in-memory/in-memory-transactions-repository.ts
@@ -1,10 +1,17 @@
-import { Prisma, $Enums, Transaction } from '@prisma/client'
-import { Decimal } from '@prisma/client/runtime/library'
+import { Prisma, Transaction } from '@prisma/client'
 import { TransactionsRepository } from '../transactions-repository'
 import { randomUUID } from 'crypto'
 
 export class InMemoryTransactionsRepository implements TransactionsRepository {
   private items: Transaction[] = []
+
+  async findManyByCurrencyId(currencyId: string, page: number) {
+    const transactions = this.items.filter(
+      (item) => item.currency_id === currencyId,
+    )
+
+    return transactions.slice((page - 1) * 7, page * 7)
+  }
 
   async create(data: Prisma.TransactionUncheckedCreateInput) {
     const transaction = {
