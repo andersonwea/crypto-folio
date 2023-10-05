519d9cacb925519f825c8004b74951612540495e
chain <klaus.mika1052@gmail.com>
2023-10-05 17:13:28 -0300

    feat: create refresh token route

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/users/register'
 import { authenticate } from './controllers/users/authenticate'
+import { refresh } from './controllers/users/refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 }
