cbcde42be1d070b015d5bb8ad7e7b25e0d786a87
chain <klaus.mika1052@gmail.com>
2023-10-05 20:10:14 -0300

    feat: create e2e tests

---
+++ server/src/http/controllers/users/profile.spec.ts
@@ -0,0 +1,30 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Porfile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get profile', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
+})
---
+++ server/src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,39 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Porfile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get profile', async () => {
+    await request(app.server).post('/users').send({
+      nickname: 'John',
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    const cookie = authResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookie)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
