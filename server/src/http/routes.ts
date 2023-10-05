479e6829d01dee0cb76ebb757b537b25cb024eb2
chain <klaus.mika1052@gmail.com>
2023-10-05 18:07:08 -0300

    feat: create profile route

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -2,10 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/users/register'
 import { authenticate } from './controllers/users/authenticate'
 import { refresh } from './controllers/users/refresh'
+import { profile } from './controllers/users/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   app.patch('/token/refresh', refresh)
+
+  /* Authenticate routes */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
