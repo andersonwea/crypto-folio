2647b8e5146a354397748fe9f8fbbbc515805715
chain <klaus.mika1052@gmail.com>
2023-10-05 16:24:38 -0300

    feat: create login route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,23 @@ import fastify from 'fastify'
 import { usersRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 
--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/users/register'
+import { authenticate } from './controllers/users/authenticate'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
