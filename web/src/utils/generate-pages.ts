70c7a0a274954dab0b428635da68e324dae0335a
chain <klaus.mika1052@gmail.com>
2023-10-20 20:40:57 -0300

    feat: create function to generates pages

---
+++ web/src/utils/generate-pages.ts
@@ -0,0 +1,32 @@
+export function generatePages(totalPages: number, activePage: number) {
+  const pages = []
+
+  if (activePage < 5) {
+    for (let page = 1; page <= 5; page++) {
+      pages.push(page)
+    }
+    pages.push(0, totalPages)
+  } else {
+    if (activePage > totalPages - 3) {
+      pages.push(1, 0)
+
+      for (let page = totalPages - 3; page <= totalPages; page++) {
+        pages.push(page)
+      }
+
+      return pages
+    }
+
+    pages.push(1, 0)
+
+    const prevPage = activePage - 1
+    const nextPage = activePage + 1
+
+    pages.push(prevPage, activePage, nextPage)
+    pages.push(0, totalPages)
+  }
+
+  return pages
+}
+
+generatePages(554, 6)
