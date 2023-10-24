bea24a1b448e847537e2742b41b1cb03554ec9f6
chain <klaus.mika1052@gmail.com>
2023-10-24 18:17:06 -0300

    refactor: add more props to currency component

--- web/src/app/wallet/components/Currency.tsx
+++ web/src/app/wallet/components/Currency.tsx
@@ -5,21 +5,34 @@ import { LiHTMLAttributes } from 'react'
 
 interface CurrencyProps extends LiHTMLAttributes<HTMLElement> {
   icon?: boolean
+  hover?: boolean
+  logoWidth?: number
+  logoHeight?: number
 }
 
-export function Currency({ icon = false }: CurrencyProps) {
+export function Currency({
+  icon = false,
+  hover = true,
+  logoHeight = 32,
+  logoWidth = 32,
+  ...props
+}: CurrencyProps) {
   return (
-    <li className="flex items-center gap-2 py-2 px-1 cursor-pointer hover:bg-gray-100 rounded">
+    <li
+      {...props}
+      className={`flex w-full items-center gap-2 py-2 px-1 cursor-pointer rounded ${
+        hover ? 'hover:bg-gray-100' : ''
+      }`}
+    >
       <Image
         src="https://img.api.cryptorank.io/coins/60x60.bitcoin1524754012028.png"
         alt="bitcoin logo"
-        width={32}
-        height={32}
+        width={logoWidth}
+        height={logoHeight}
       />
-      <Text as="p">
+      <Text as="p" className="w-full">
         Bitcoin <Text color="gray">BTC</Text>
       </Text>
-
       {icon && (
         <b className="ml-auto">
           <ChevronRight />
