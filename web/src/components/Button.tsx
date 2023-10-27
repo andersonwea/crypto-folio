b8c66e338a2e7630867218c3b83e61b114e12a69
chain <klaus.mika1052@gmail.com>
2023-10-27 17:29:07 -0300

    feat: add styles when disabled

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -8,7 +8,7 @@ export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-blue-300 h-10 cursor-pointer rounded-full max-w-full px-4 hover:brightness-95 transition-all text-gray-800"
+      className="bg-blue-300 h-10 cursor-pointer rounded-full max-w-full px-4 hover:brightness-95 transition-all text-gray-800 disabled:bg-blue-100 disabled:pointer-events-none"
     >
       {children}
     </button>
