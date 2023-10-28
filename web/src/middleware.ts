e957e37eb7f18fe85399793e8a9ac6df1a92e536
chain <klaus.mika1052@gmail.com>
2023-10-28 12:54:41 -0300

    feat: create middleware to protect routes

---
+++ web/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+
+  const homeUrl = new URL(request.url).hostname
+
+  if (!token) {
+    return NextResponse.redirect(`http://${homeUrl}:3000`, {
+      headers: {
+        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
+      },
+    })
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/wallet/:path*',
+    '/me/:path*',
+    '/market/:path*',
+    '/dashboard/:path*',
+    '/watchlist/:path*',
+  ],
+}
