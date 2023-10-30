815be68681da3cd7b7ebc921c280b8e8394a70c2
chain <klaus.mika1052@gmail.com>
2023-10-30 12:30:01 -0300

    refactor: add ref to component

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,16 +1,22 @@
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, Ref, forwardRef } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string
+  ref: Ref<HTMLButtonElement>
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export const Button = forwardRef(function Button({
+  children,
+  ref,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
+      ref={ref}
       className="bg-blue-300 h-10 cursor-pointer rounded-full max-w-full px-4 hover:brightness-95 transition-all text-gray-800 disabled:bg-blue-100 disabled:pointer-events-none"
     >
       {children}
     </button>
   )
-}
+})
