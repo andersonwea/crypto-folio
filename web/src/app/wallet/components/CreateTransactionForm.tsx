fe28f1bce708fa5807c8f09f2ba97a56cb2a8247
chain <klaus.mika1052@gmail.com>
2023-11-01 18:21:53 -0300

    feat: implements transaction store as for create transaction

--- web/src/app/wallet/components/CreateTransactionForm.tsx
+++ web/src/app/wallet/components/CreateTransactionForm.tsx
@@ -2,14 +2,30 @@
 
 import { Button } from '@/components/Button'
 import { TextInput } from '@/components/TextInput'
-import { Heading, ScrollArea, Select, Text } from '@radix-ui/themes'
-import { Currency } from './Currency'
+import { ScrollArea, Select, Text } from '@radix-ui/themes'
+import { CurrencyItem } from './CurrencyItem'
 import dayjs from 'dayjs'
-import { useSelectCurrency } from '@/hooks/useSelectCurrency'
+import { useCurrencyStore } from '@/store/useCurrencyStore'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import { useTransactionStore } from '@/store/useTransactionsStore'
+import { useCallback } from 'react'
 
 interface CreateTransactionFormProps {
   transactionType: string
 }
+
+const createTransactionFormSchema = z.object({
+  amount: z.number().min(0.001, { message: 'Transação de no mínimo 0.001' }),
+  currencyPrice: z.number().min(0.001, { message: 'Preço de no mínimo 0.001' }),
+  createdAt: z.coerce
+    .date()
+    .max(new Date(), { message: 'Data futura não permitida.' }),
+})
+
+type CreateTransactionForm = z.infer<typeof createTransactionFormSchema>
 
 function getActualDate() {
   const today = dayjs(new Date())
@@ -20,18 +36,86 @@ function getActualDate() {
 export function CreateTransactionForm({
   transactionType,
 }: CreateTransactionFormProps) {
-  const currency = useSelectCurrency((state) => state.currency)
+  const selectedMarketCurrency = useCurrencyStore(
+    (state) => state.selectedMarketCurrency,
+  )
+  const marketCurrencies = useCurrencyStore((state) => state.marketCurrencies)
+  const createTransaction = useTransactionStore(
+    useCallback((state) => state.createTransaction, []),
+  )
+  const createWalletCurrency = useCurrencyStore(
+    useCallback((state) => state.createWalletCurrency, []),
+  )
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<CreateTransactionForm>({
+    resolver: zodResolver(createTransactionFormSchema),
+    defaultValues: {
+      amount: 0,
+      currencyPrice: selectedMarketCurrency?.values.price ?? 0,
+    },
+  })
+
+  const router = useRouter()
+
+  const amount = watch('amount')
+  const currencyPrice = watch('currencyPrice')
+
+  async function handleCreateTransaction(data: CreateTransactionForm) {
+    if (!selectedMarketCurrency) {
+      return alert('Selecione uma moeda') // TODO add toastify lib
+    }
+
+    const { amount, createdAt } = data
+    const { id, name, symbol, image } = selectedMarketCurrency
+    const value = amount * currencyPrice
+
+    const currency = await createWalletCurrency({
+      amount,
+      cryptocurrencyId: id,
+      image,
+      name,
+      symbol,
+    })
+
+    if (!currency) {
+      return alert('Falha ao criar transação.') // TODO add toastify lib
+    }
+
+    await createTransaction(
+      {
+        amount,
+        createdAt,
+        type: transactionType,
+        value,
+      },
+      currency.id,
+    )
+
+    router.refresh()
+  }
 
   return (
-    <form className="pt-10 flex flex-col">
-      <Select.Root defaultValue={currency} size={'3'}>
+    <form
+      className="pt-10 flex flex-col"
+      onSubmit={handleSubmit(handleCreateTransaction)}
+    >
+      <Select.Root defaultValue={String(selectedMarketCurrency?.id)} size={'3'}>
         <Select.Trigger radius="large" color="gray" />
 
         <Select.Content color="gray">
           <ScrollArea type="auto" scrollbars="vertical" style={{ height: 200 }}>
-            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-              <Select.Item value={`bitcoin${item}`} key={item}>
-                <Currency
+            {marketCurrencies.map((marketCurrency) => (
+              <Select.Item
+                value={String(marketCurrency.id)}
+                key={marketCurrency.id}
+              >
+                <CurrencyItem
+                  currency={marketCurrency}
                   icon={false}
                   hover={false}
                   logoHeight={24}
@@ -46,25 +130,60 @@ export function CreateTransactionForm({
       <div className="flex gap-4 pt-4">
         <label htmlFor="" className="w-full space-y-1">
           <Text>Quantidade</Text>
-          <TextInput placeholder="0.00" />
+          <TextInput
+            placeholder="0.00"
+            type="number"
+            {...register('amount', { valueAsNumber: true })}
+          />
+
+          {errors.amount && (
+            <Text color="red" size={'2'}>
+              {errors.amount.message}
+            </Text>
+          )}
         </label>
 
         <label htmlFor="" className="w-full space-y-1">
           <Text>Preço por moeda</Text>
-          <TextInput defaultValue={'34000,09'} placeholder="0.00" />
+          <TextInput
+            placeholder="0.00"
+            {...register('currencyPrice', { valueAsNumber: true })}
+          />
+
+          {errors.currencyPrice && (
+            <Text color="red" size={'2'}>
+              {errors.currencyPrice.message}
+            </Text>
+          )}
         </label>
       </div>
 
       <div className="max-w-[220px] mt-4">
-        <TextInput type="date" defaultValue={getActualDate()} />
+        <TextInput
+          type="date"
+          defaultValue={getActualDate()}
+          {...register('createdAt')}
+        />
+
+        {errors.createdAt && (
+          <Text color="red" size={'2'}>
+            {errors.createdAt.message}
+          </Text>
+        )}
       </div>
 
-      <div className="bg-gray-300 my-8 rounded-md py-3 px-4 space-y-4">
+      <label className="bg-gray-300 my-8 rounded-md py-3 px-4 space-y-4">
         <Text>Total gasto</Text>
-        <Heading as="h2">$ 25,588.30</Heading>
-      </div>
+        <input
+          className="bg-transparent font-bold text-2xl block outline-0"
+          readOnly={true}
+          value={amount * currencyPrice}
+        />
+      </label>
 
-      <Button>Add transação</Button>
+      <Button disabled={isSubmitting} type="submit">
+        Add transação
+      </Button>
     </form>
   )
 }
