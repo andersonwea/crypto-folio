f9fd703c7815c4ce89bd5d0bfa9a80e3ae640297
chain <klaus.mika1052@gmail.com>
2023-11-02 14:31:08 -0300

    refactor: change component name

---
+++ web/src/app/wallet/components/CurrencyItem.tsx
@@ -0,0 +1,46 @@
+import { MarketCurrency } from '@/@types'
+import { Text } from '@radix-ui/themes'
+import { ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+import { LiHTMLAttributes } from 'react'
+
+interface CurrencyProps extends LiHTMLAttributes<HTMLElement> {
+  icon?: boolean
+  hover?: boolean
+  logoWidth?: number
+  logoHeight?: number
+  currency: MarketCurrency
+}
+
+export function CurrencyItem({
+  icon = false,
+  hover = true,
+  logoHeight = 32,
+  logoWidth = 32,
+  currency,
+  ...props
+}: CurrencyProps) {
+  return (
+    <li
+      {...props}
+      className={`flex w-full items-center gap-2 py-2 px-1 cursor-pointer rounded ${
+        hover ? 'hover:bg-gray-100' : ''
+      }`}
+    >
+      <Image
+        src={currency.image}
+        alt="bitcoin logo"
+        width={logoWidth}
+        height={logoHeight}
+      />
+      <Text as="p" className="w-full">
+        {currency.name} <Text color="gray">{currency.symbol}</Text>
+      </Text>
+      {icon && (
+        <b className="ml-auto">
+          <ChevronRight />
+        </b>
+      )}
+    </li>
+  )
+}
