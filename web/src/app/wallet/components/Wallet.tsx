456977d6319c6bcc64078a2c3bc6fb231c4a6b43
chain <klaus.mika1052@gmail.com>
2023-11-04 20:53:59 -0300

    feat: create search filter

--- web/src/app/wallet/components/Wallet.tsx
+++ web/src/app/wallet/components/Wallet.tsx
@@ -3,7 +3,7 @@
 import { Heading, ScrollArea } from '@radix-ui/themes'
 import { Card } from './Card'
 import { TextInput } from '@/components/TextInput'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { EmptyCard } from './EmptyCard'
 import { useCurrencyStore } from '@/store/useCurrencyStore'
 import { useTransactionStore } from '@/store/useTransactionsStore'
@@ -15,6 +15,7 @@ interface WalletProps {
 const colors = ['bg-green-300', 'bg-purple-300', 'bg-yellow-300']
 
 export function Wallet({ maxWidth }: WalletProps) {
+  const [search, setSearch] = useState('')
   const transactions = useTransactionStore((state) => state.transactions)
   const fetchWalletCurrencies = useCurrencyStore(
     useCallback((state) => state.fetchWalletCurrencies, [transactions]),
@@ -22,6 +23,10 @@ export function Wallet({ maxWidth }: WalletProps) {
   const walletCurrencies = useCurrencyStore(
     useCallback((state) => state.walletCurrencies, []),
   )
+
+  function onSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+  }
 
   useEffect(() => {
     fetchWalletCurrencies()
@@ -31,7 +36,7 @@ export function Wallet({ maxWidth }: WalletProps) {
     <section className="pt-4">
       <div className="flex items-center gap-4 max-sm:flex-col max-sm:items-start max-sm:gap-2">
         <Heading as="h2">Seus Ativos</Heading>
-        <TextInput placeholder="Filtrar ativos" />
+        <TextInput placeholder="Filtrar ativos" onChange={onSearch} />
       </div>
 
       <ScrollArea
@@ -44,15 +49,17 @@ export function Wallet({ maxWidth }: WalletProps) {
             // currencies && currencies.length < 1 ? (
             //   <EmptyCard />
             // ) : (
-
-            walletCurrencies.map((walletCurrency, index) => (
-              <Card
-                key={walletCurrency.id}
-                currency={walletCurrency}
-                color={colors[index % colors.length]}
-              />
-            ))
-            // )
+            walletCurrencies
+              .filter((walletCurrency) =>
+                walletCurrency.name.includes(search.toLocaleLowerCase()),
+              )
+              .map((walletCurrency, index) => (
+                <Card
+                  key={walletCurrency.id}
+                  currency={walletCurrency}
+                  color={colors[index % colors.length]}
+                />
+              ))
           }
         </div>
       </ScrollArea>
