b6b6bb8a14e8360b99deed40f93de6978b4dd2de
chain <klaus.mika1052@gmail.com>
2023-11-09 14:36:09 -0300

    feat: create test e2e for search cryptocurrencies route

---
+++ server/src/http/controllers/currencies/search-cryptocurrencies.spec.ts
@@ -0,0 +1,57 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Search Cryptocurrencies (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search cryptocurrencies', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await prisma.cryptocurrency.createMany({
+      data: [
+        {
+          id: 1,
+          name: 'bitcoin',
+          symbol: 'BTC',
+          image: 'btc-image',
+        },
+        {
+          id: 2,
+          name: 'ethereum',
+          symbol: 'ETH',
+          image: 'ethereum-image',
+        },
+        {
+          id: 3,
+          name: 'solana',
+          symbol: 'SOL',
+          image: 'solana-image',
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/market/currencies/search')
+      .set('Authorization', `Bearer ${token}`)
+      .query({
+        search: 'bitcoin',
+      })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual([
+      expect.objectContaining({
+        name: 'bitcoin',
+        symbol: 'BTC',
+      }),
+    ])
+  })
+})
