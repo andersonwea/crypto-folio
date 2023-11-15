4f02199f66da83dcc4fcd0fb8d8bd5c53ca0a37a
chain <klaus.mika1052@gmail.com>
2023-11-15 11:59:44 -0300

    refact: config protect routes

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -50,6 +50,7 @@ export const authOptions = {
 
   pages: {
     signIn: '/',
+    signOut: '/',
   },
 
   callbacks: {
--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,20 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('cryptofolio.token')?.value
-
-  const homeUrl = new URL(request.url).hostname
-
-  if (!token) {
-    return NextResponse.redirect(`http://${homeUrl}:3000`, {
-      headers: {
-        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
-      },
-    })
-  }
-
-  return NextResponse.next()
-}
+export { default } from 'next-auth/middleware'
 
 export const config = {
   matcher: [
