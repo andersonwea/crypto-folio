21b0ec77140cefde56d110043c011f058600ae99
chain <klaus.mika1052@gmail.com>
2023-11-15 12:06:26 -0300

    refact: use action to fetch data

--- web/src/app/wallet/layout.tsx
+++ web/src/app/wallet/layout.tsx
@@ -1,18 +1,21 @@
 import { Header } from '@/components/Header'
 import { Wallet } from './components/Wallet'
+import { getWalletCurrencies } from '@/actions/getWalletCurrencies'
 
-export default function WalletLayout({
+export default async function WalletLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { walletCurrencies } = await getWalletCurrencies()
+
   return (
     <div>
       <Header title="Portfolio" />
 
       <main className="grid grid-cols-[0.6fr_minmax(0,_1fr)] max-md:grid-cols-1">
         <section className="col-span-2 max-md:col-span-1">
-          <Wallet />
+          {walletCurrencies && <Wallet walletCurrencies={walletCurrencies} />}
         </section>
 
         {children}
--- web/src/components/MenuNav.tsx
+++ web/src/components/MenuNav.tsx
@@ -1,22 +1,10 @@
-'use client'
-
 import Link from 'next/link'
 import { Nav } from './Nav'
-import { LogOut } from 'lucide-react'
 import Image from 'next/image'
 import logo from '@/assets/logo.svg'
-import cookies from 'js-cookie'
-import { useRouter } from 'next/navigation'
+import { SignOutButton } from './SignOutButton'
 
 export function MenuNav() {
-  const router = useRouter()
-
-  function signOut() {
-    cookies.remove('cryptofolio.token')
-
-    router.refresh()
-  }
-
   return (
     <div className="flex flex-col items-center lg:mt-6 lg:mb-6 max-lg:flex-row max-lg:row-start-2 max-lg:justify-evenly max-[450px]:justify-between max-[450px]:px-4">
       <Link href={'/'} className="max-md:hidden">
@@ -25,9 +13,7 @@ export function MenuNav() {
 
       <Nav />
 
-      <button className="h-6 w-6 text-[#A5A5A5]" onClick={() => signOut()}>
-        <LogOut size={26} />
-      </button>
+      <SignOutButton />
     </div>
   )
 }
