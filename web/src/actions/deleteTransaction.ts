3e0923ce8481670166bb7956081f4e7c28878a6e
chain <klaus.mika1052@gmail.com>
2023-11-15 14:20:38 -0300

    feat: create action to delete transaction

---
+++ web/src/actions/deleteTransaction.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { api } from '@/libs/api'
+import { AxiosError } from 'axios'
+import { getServerSession } from 'next-auth'
+import { revalidatePath } from 'next/cache'
+
+export async function deleteTransaction(
+  transactionId: string,
+  currencyId: string,
+) {
+  const session = await getServerSession(authOptions)
+
+  try {
+    await api.delete(
+      `/wallet/currencies/${currencyId}/transactions/${transactionId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${session?.user.accessToken}`,
+        },
+      },
+    )
+
+    revalidatePath('/wallet')
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      return {
+        deleteTransactionError: err.response?.data.message as string,
+      }
+    }
+
+    return {
+      error: err,
+    }
+  }
+}
