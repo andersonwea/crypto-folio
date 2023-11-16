24898b0c02b48efddcf31d4eeecd2f38573f5883
chain <klaus.mika1052@gmail.com>
2023-11-16 15:21:30 -0300

    feat: create server action to create transaction

---
+++ web/src/actions/addTransaction.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import { NewTransactionInput, Transaction } from '@/@types'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { api } from '@/libs/api'
+import { AxiosError } from 'axios'
+import { getServerSession } from 'next-auth'
+import { revalidatePath } from 'next/cache'
+
+export async function addTransaction(
+  data: NewTransactionInput,
+  currencyId: string,
+) {
+  const session = await getServerSession(authOptions)
+
+  try {
+    const response = await api.post<Transaction>(
+      `/wallet/currencies/${currencyId}/transactions`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${session?.user.accessToken}`,
+        },
+      },
+    )
+
+    const transaction = response.data
+
+    revalidatePath('/wallet')
+
+    return {
+      transaction,
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      console.log(err.response?.data.issues)
+      return {
+        addTransactionError: err.response?.data.message as string,
+      }
+    }
+  }
+}
