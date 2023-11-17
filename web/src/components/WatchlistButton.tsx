05131b3d460d219edfac1c307716e26fbfa43127
chain <klaus.mika1052@gmail.com>
2023-11-17 12:43:37 -0300

    feat: create button to add crypto to watchlist

---
+++ web/src/components/WatchlistButton.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { MarketCurrency } from '@/@types'
+import { toggleWatchlist } from '@/actions/toggleWatchlist'
+import { Star } from 'lucide-react'
+import { experimental_useOptimistic as useOptimistic } from 'react'
+
+interface WatchlistButtonProps {
+  marketCurrency: MarketCurrency
+  watchlist: MarketCurrency[]
+}
+
+export function WatchlistButton({
+  marketCurrency,
+  watchlist,
+}: WatchlistButtonProps) {
+  const [optimisticWatchlist, addOptimisticWatchlist] = useOptimistic(
+    watchlist,
+    (state, newWatchlist: MarketCurrency) => {
+      if (state.find((watchlist) => watchlist.id === newWatchlist.id)) {
+        return state.filter((watchlist) => watchlist.id !== newWatchlist.id)
+      }
+
+      return [...state, newWatchlist]
+    },
+  )
+
+  async function handleToggleWatchlist(marketCurrency: MarketCurrency) {
+    addOptimisticWatchlist(marketCurrency)
+
+    await toggleWatchlist(marketCurrency.id)
+  }
+
+  const isWatchlisted = !!optimisticWatchlist?.find(
+    (watchlist) => watchlist.id === marketCurrency.id,
+  )
+
+  return (
+    <Star
+      onClick={() => handleToggleWatchlist(marketCurrency)}
+      data-watchlist={isWatchlisted}
+      size={20}
+      className="data-[watchlist=true]:fill-blue-500 cursor-pointer"
+      stroke={isWatchlisted ? '#0587FF' : 'gray'}
+    />
+  )
+}
