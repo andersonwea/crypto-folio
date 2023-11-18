ebdb8b056b109c9ebd37b12af826ea8b2e44c4a6
chain <klaus.mika1052@gmail.com>
2023-11-18 12:54:42 -0300

    refact: change field props name

--- web/src/app/wallet/components/Card.tsx
+++ web/src/app/wallet/components/Card.tsx
@@ -6,33 +6,36 @@ import Link from 'next/link'
 
 interface CardProps {
   color: string
-  currency: WalletCurrency
+  walletCurrency: WalletCurrency
 }
 
-export function Card({ color, currency }: CardProps) {
+export function Card({ color, walletCurrency }: CardProps) {
   return (
     <Link
-      className={`${color} rounded-3xl px-9 py-6 min-h-[222px] flex flex-col justify-between max-w-[194px]`}
-      href={`/wallet/currency/${currency.id}`}
+      className={`${color} rounded-3xl px-6 py-6 min-h-[222px] flex flex-col justify-between min-w-[194px]`}
+      href={`/wallet/currency/${walletCurrency.id}`}
     >
       <div>
         <Heading>
-          {currency.amount} <span>{currency.symbol}</span>
+          {walletCurrency.amount} <span>{walletCurrency.symbol}</span>
         </Heading>
-        <Text>{priceFormatter.format(currency.balance)}</Text>
+        <Text>{priceFormatter.format(walletCurrency.balance)}</Text>
       </div>
 
       <div className="flex items-center space-x-4">
         <div className="w-14 h-14 bg-white rounded-2xl flex items-center justify-center">
           <Image
-            src={currency.image}
+            src={walletCurrency.image}
             alt="bitcoin logo"
             width={32}
             height={32}
           />
         </div>
-        <Text as="span" color="green">
-          0.15% {/** //TODO change this */}
+        <Text
+          as="span"
+          color={walletCurrency.profitOrLoss < 0 ? 'red' : 'green'}
+        >
+          {walletCurrency.profitOrLoss}%
         </Text>
       </div>
     </Link>
