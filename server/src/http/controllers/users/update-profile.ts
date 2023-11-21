018108c926801cbaba465bc97ed4e7a82b7bb709
chain <klaus.mika1052@gmail.com>
2023-11-21 18:19:06 -0300

    fix: error handler

--- server/src/http/controllers/users/update-profile.ts
+++ server/src/http/controllers/users/update-profile.ts
@@ -15,16 +15,24 @@ export async function updateProfile(
 
   const { avatarUrl, nickname } = updateProfileBodySchema.parse(request.body)
 
-  const updateUserProfileUseCase = makeUpdateUserProfileUseCase()
+  try {
+    const updateUserProfileUseCase = makeUpdateUserProfileUseCase()
 
-  const { updatedUser } = await updateUserProfileUseCase.execute({
-    nickname,
-    userId,
-    avatarUrl,
-  })
+    const { updatedUser } = await updateUserProfileUseCase.execute({
+      nickname,
+      userId,
+      avatarUrl,
+    })
 
-  return reply.status(200).send({
-    ...updatedUser,
-    password_hash: undefined,
-  })
+    return reply.status(200).send({
+      ...updatedUser,
+      password_hash: undefined,
+    })
+  } catch (err) {
+    if (err instanceof Error) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
 }
