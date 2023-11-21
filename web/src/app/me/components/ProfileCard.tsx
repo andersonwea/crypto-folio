3c23bf28b894c93fa6d250e56c925c463e451b51
chain <klaus.mika1052@gmail.com>
2023-11-21 14:30:46 -0300

    refact: create a profile card with user informations

---
+++ web/src/app/me/components/ProfileCard.tsx
@@ -0,0 +1,43 @@
+import { getWalletStats } from '@/actions/getWalletStats'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { priceFormatter } from '@/utils/priceFormatter'
+import { Avatar, Heading, Text } from '@radix-ui/themes'
+import { getServerSession } from 'next-auth'
+
+export async function ProfileCard() {
+  const session = await getServerSession(authOptions)
+  const { metrics } = await getWalletStats()
+
+  return (
+    <div className="flex justify-start max-md:justify-center max-md:row-start-1 items-center flex-wrap gap-4 pt-10">
+      <div className="flex flex-col items-center border rounded-lg py-6 px-12 gap-3">
+        <Avatar
+          radius="full"
+          color="orange"
+          variant="solid"
+          fallback={'A'}
+          src={session?.user.avatarUrl ?? ''}
+          size={{
+            initial: '7',
+            xs: '8',
+            sm: '8',
+            md: '8',
+          }}
+        />
+        <div>
+          <Heading as="h2" size={'5'} className="text-center">
+            {session?.user.nickname}
+          </Heading>
+          <Text color="gray">{session?.user.email}</Text>
+        </div>
+
+        <div className="flex flex-col mt-8 self-start">
+          <Text color="gray">Balance</Text>
+          <Text>
+            {metrics && priceFormatter.format(metrics?.totalBalance)} 🚀
+          </Text>
+        </div>
+      </div>
+    </div>
+  )
+}
--- web/src/app/me/page.tsx
+++ web/src/app/me/page.tsx
@@ -1,13 +1,9 @@
 import { Header } from '@/components/Header'
-import { Avatar, Grid, Heading, Text } from '@radix-ui/themes'
+import { Grid, Heading } from '@radix-ui/themes'
 import { UpdateProfileForm } from './components/UpdateProfileForm'
-import { UpdatePrivacyProfileForm } from './components/UpdatePrivacyProfileForm'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
+import { ProfileCard } from './components/ProfileCard'
 
 export default async function Profile() {
-  const session = await getServerSession(authOptions)
-
   return (
     <div>
       <Header title="Meu perfil" />
@@ -15,34 +11,14 @@ export default async function Profile() {
       <main className="pt-7">
         <Heading>Atualizar perfil</Heading>
 
-        <div className="flex items-center flex-wrap gap-4 pt-10">
-          <Avatar
-            className=""
-            radius="full"
-            color="orange"
-            variant="solid"
-            fallback={'A'}
-            src={session?.user.avatarUrl ?? ''}
-            size={{
-              initial: '4',
-              sm: '5',
-              md: '7',
-            }}
-          />
-          <div>
-            <Heading as="h2" size={'5'}>
-              {session?.user.nickname}
-            </Heading>
-            <Text color="gray">{session?.user.email}</Text>
-          </div>
-        </div>
-
         <Grid
+          className="mt-8"
           gap={{ initial: '5', sm: '6', lg: '8' }}
           columns={{ initial: '1', sm: '2' }}
         >
           <UpdateProfileForm />
-          <UpdatePrivacyProfileForm />
+          <ProfileCard />
+          {/* <UpdatePrivacyProfileForm /> */}
         </Grid>
       </main>
     </div>
