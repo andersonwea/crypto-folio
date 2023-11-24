7fdb4453183aabfbe9e859fae7a80f3937a801ca
chain <klaus.mika1052@gmail.com>
2023-11-24 16:31:10 -0300

    feat: install and configure nextUi

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react'
 import { Theme } from '@radix-ui/themes'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
+import { NextUIProvider } from '@nextui-org/react'
 
 interface ProvidersProps {
   children: ReactNode
@@ -13,7 +14,9 @@ interface ProvidersProps {
 export function Providers({ children, session }: ProvidersProps) {
   return (
     <SessionProvider session={session}>
-      <Theme>{children}</Theme>
+      <NextUIProvider>
+        <Theme>{children}</Theme>
+      </NextUIProvider>
     </SessionProvider>
   )
 }
