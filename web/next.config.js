43d422236356e647176ba79aaf78c1785b2ca430
chain <klaus.mika1052@gmail.com>
2024-02-05 13:39:12 -0300

    fix: add images domain to next config

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.api.cryptorank.io'],
+    domains: ['img.api.cryptorank.io', 'img.cryptorank.io'],
   },
   experimental: {
     serverActions: true,
